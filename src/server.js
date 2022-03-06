77f0ab15c3a78172c7e6ad8211b559505d299865
JooMercury <91738673+JooMercury@users.noreply.github.com>
2022-03-06 20:05:12 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const PORT = 4000;
 
 const app = express();
 
+app.get("/", () => console.log("somebody is trying to go home."));
+
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
