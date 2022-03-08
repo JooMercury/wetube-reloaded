1c664551a2400b6e8d4cd7248911dca83c8ed2fa
JooMercury <91738673+JooMercury@users.noreply.github.com>
2022-03-08 19:12:23 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,13 +4,18 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to : ${req.url}`);
+  next();
+};
+
 const handleHome = (req, res) => {
-  return res.send("<h1>I still love you.😍</h1>");
+  return res.send("<h1>I love middlewares.😍</h1>");
 };
 const handleLogin = (req, res) => {
   return res.send({ message: "Login here." });
 };
-app.get("/", handleHome);
+app.get("/", gossipMiddleware, handleHome);
 app.get("/login", handleLogin);
 
 const handleListening = () =>
