cb4de5d251233ccfbebe5aeb09bf81776d970c96
JooMercury <91738673+JooMercury@users.noreply.github.com>
2022-03-09 08:04:29 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,18 +4,29 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to : ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
   next();
 };
 
-const handleHome = (req, res) => {
+const privateMiddleware = (req, res) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed </h1>");
+  }
+  next();
+  console.log("Allowed, you may continue.");
+};
+
+const handleHome = (req, res, next) => {
   return res.send("<h1>I love middlewares.😍</h1>");
 };
+
 const handleLogin = (req, res) => {
   return res.send({ message: "Login here." });
 };
-app.get("/", gossipMiddleware, handleHome);
+
+app.get("/", logger, handleHome);
 app.get("/login", handleLogin);
 
 const handleListening = () =>
