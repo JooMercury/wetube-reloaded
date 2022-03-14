16d56db0f607ca9e3a4411fe3990158445b27348
JooMercury <91738673+JooMercury@users.noreply.github.com>
2022-03-14 12:26:08 +0900

    update variables to template

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,8 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("Edit");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) =>
+  res.render("watch", { pageTitle: "Watch Videos" });
+export const edit = (req, res) =>
+  res.render("Edit", { pageTitle: "Edit Videos" });
 
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
