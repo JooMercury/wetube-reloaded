18da48864b48895427e63c7fdc6447186bb538d2
JooMercury <91738673+JooMercury@users.noreply.github.com>
2022-04-10 19:41:52 +0900

    Creating a Video # 02

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -23,18 +23,17 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  const video = new Video({
+  await Video.create({
     title: title,
     description: description,
-    createdAt: Date.now(),
+    createdAt: "lalalalalal",
     hashtags: hashtags.split(",").map((word) => `#${word}`),
     meta: {
       views: 0,
       rating: 0,
     },
   });
-  console.log(video);
   return res.redirect("/");
 };
