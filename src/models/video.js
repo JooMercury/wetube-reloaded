5fbf996f79aeeb26f4cbbda6315fc4a3f97b049e
JooMercury <91738673+JooMercury@users.noreply.github.com>
2022-07-29 16:45:49 +0900

    User's Videos

--- src/models/video.js
+++ src/models/video.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxLength: 80 },
   fileUrl: { type: String, reqired: true },
-  description: { type: String, required: true, trim: true, maxLength: 20 },
+  description: { type: String, required: true, trim: true, minLength: 20 },
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trim: true }],
   meta: {
